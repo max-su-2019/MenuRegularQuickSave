3bb5ab4c3cecf9e6a24ceef12769cdffd22c2192
max-su-2019 <54713650+max-su-2019@users.noreply.github.com>
2023-04-30 11:22:38 +0800

    Update to v1.01

--- src/Hook.cpp
+++ src/Hook.cpp
@@ -4,7 +4,8 @@ namespace QuickSave
 {
 	void SystemQuickSaveHook::ProcessQuickSave(RE::BGSSaveLoadManager* a_manager, std::uint32_t a_flag)
 	{
-		static constexpr std::uint32_t regular_save = 0xF0000080;
-		return _ProcessQuickSave(a_manager, regular_save);
+		static constexpr std::uint32_t button_regular_save = 0xF0000080;
+		_ProcessQuickSave(a_manager, button_regular_save);
+		return RE::UIMessageQueue::GetSingleton()->AddMessage(RE::JournalMenu::MENU_NAME, RE::UI_MESSAGE_TYPE::kHide, nullptr);
 	}
 }
